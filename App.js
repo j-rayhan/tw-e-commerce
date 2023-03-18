6f8ee6f753974b0a17846ac956466c8f18dca6a0
bss-johir <bss.johir@gmail.com>
2023-03-18 22:52:54 +0600

    Switch theme

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import { StyleSheet, Switch, Text, View } from 'react-native';
 
 export default function App() {
+  const {colorScheme, toggleColorScheme} = useColorScheme()
   return (
-    <View className="flex-1 justify-center items-center bg-sky-400">
-      <Text className="font-semibold text-xl">Open up App.js!</Text>
+    <View className="flex-1 justify-center items-center bg-sky-400 dark:bg-black">
+      <Switch value={colorScheme === 'dark'} onChange={() => toggleColorScheme()} />
+      <Text className="font-semibold text-xl dark:text-white">Open up App.js!</Text>
       <StatusBar style="auto" />
     </View>
   );
