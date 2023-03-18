f00143e869247781414401ffd46d0996d50b1a9d
bss-johir <bss.johir@gmail.com>
2023-03-18 22:43:40 +0600

    Add lib

--- App.js
+++ App.js
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js!</Text>
+    <View className="flex-1 justify-center items-center bg-sky-400">
+      <Text className="font-semibold text-xl">Open up App.js!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
