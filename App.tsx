431e2f9adfad60d2c038bad6ae6afff6b8d5a044
bss-johir <bss.johir@gmail.com>
2023-03-21 08:20:07 +0600

    FIX: Animation Layout

--- App.tsx
+++ App.tsx
@@ -28,7 +28,7 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import {useColorScheme} from 'nativewind';
 import ProductList from './src/components/ProductList';
-import Animated, {FadeIn, FadeInUp, Layout} from 'react-native-reanimated';
+import Animated, {FadeIn, FadeInUp, FadeOut, Layout} from 'react-native-reanimated';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -43,7 +43,10 @@ function ItemList(): JSX.Element {
   );
 
   const addItem = () => {
-    setItems(prevItems => [...prevItems, {id: Date.now() + ''}]);
+    setItems(prevItems => [{id: Date.now() + ''}, ...prevItems]);
+  };
+  const deleteItem = (itemID: string) => {
+    setItems(prevItems => prevItems.filter(({id}) => id !== itemID));
   };
   console.log('PRINT IN %s=====>', 'App START ***', items);
   return (
@@ -64,7 +67,10 @@ function ItemList(): JSX.Element {
       {items.map(({id}) => (
         <Animated.View
           key={id}
+          entering={FadeIn}
+          exiting={FadeOut}
           layout={Layout}
+          onTouchEnd={() => deleteItem(id)}
           className="w-11/12 h-24 self-center rounded-2xl my-2 bg-blue-400">
           <Text className="text-center mt-7 text-2xl">{id}</Text>
         </Animated.View>
@@ -104,4 +110,4 @@ function App(): JSX.Element {
   );
 }
 
-export default App;
+export default ItemList;
