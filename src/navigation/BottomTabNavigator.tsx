b34a1e5fe1396f371b5ff8106bd8d22dcf0f266e
bss-johir <bss.johir@gmail.com>
2023-03-25 15:45:51 +0600

    Custom Tab Bar

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,5 +1,7 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import type {BottomTabBarProps} from '@react-navigation/bottom-tabs';
 import * as React from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {SCREEN} from '../constants/constant';
 import {Home, Notification, Setting, Wallet} from '../screens/bottom_tab';
 
@@ -10,11 +12,63 @@ export type BottomStackParamList = {
   [SCREEN.SETTING]: undefined;
 };
 
+function CustomTabBar({state, descriptors, navigation}: BottomTabBarProps) {
+  return (
+    <View className="flex-row items-center p-2 py-4">
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            className="p-2"
+            accessibilityRole="button"
+            accessibilityState={isFocused ? {selected: true} : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{flex: 1}}>
+            <Text style={{color: isFocused ? '#673ab7' : '#222'}}>{label}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
 const Tab = createBottomTabNavigator<BottomStackParamList>();
 
 function BottomTabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{headerShown: false}}
+      tabBar={props => <CustomTabBar {...props} />}>
       <Tab.Screen name={SCREEN.HOME} component={Home} />
       <Tab.Screen name={SCREEN.WALLET} component={Wallet} />
       <Tab.Screen name={SCREEN.NOTIFICATION} component={Notification} />
